41f7a005811f4774a84899c418d5221db7005b9c
haisumb <haisum@boxboat.com>
2020-03-11 12:48:05 -0400

    new version

--- app/src/app.py
+++ app/src/app.py
@@ -16,12 +16,12 @@ def error500():
 
 @app.route('/')
 def hello_world():
-    return 'Flask: Hello World from Canary Deployment v13'
+    return 'Flask: Hello World from Canary Deployment v14'
 
 
 @app.route('/version')
 def version():
-    return 'V13'
+    return 'V14'
 
 
 @app.route('/log')
