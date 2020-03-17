f2f1de721708303e4a8788e2b376d869e0c62566
haisumb <haisum@boxboat.com>
2020-03-17 09:20:06 -0400

    indentation to spaces

--- app/src/app.py
+++ app/src/app.py
@@ -13,12 +13,12 @@ log.setLevel(logging.ERROR)
 
 @app.route('/error/500')
 def error500():
-	abort(500, description="internal server error")
+    abort(500, description="internal server error")
 
 @app.route('/')
 def hello_world():
-	# Add latency
-	time.sleep(2)
+    # Add latency
+    time.sleep(2)
     return 'Flask: Hello World from Canary Deployment v19'
 
 
